04c39abdca4d8712ec4e364cf49e9daad3636981
Ivan Rep <ivan.rep2@gmail.com>
2017-02-05 12:52:18 +0100

    Initial Commit

---
+++ CUPUS_iOS_MobileBroker/CUPUS_iOS_MobileBroker.h
@@ -0,0 +1,19 @@
+//
+//  CUPUS_iOS_MobileBroker.h
+//  CUPUS_iOS_MobileBroker
+//
+//  Created by Ivan Rep on 05/02/17.
+//  Copyright © 2017 CUPUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CUPUS_iOS_MobileBroker.
+FOUNDATION_EXPORT double CUPUS_iOS_MobileBrokerVersionNumber;
+
+//! Project version string for CUPUS_iOS_MobileBroker.
+FOUNDATION_EXPORT const unsigned char CUPUS_iOS_MobileBrokerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CUPUS_iOS_MobileBroker/PublicHeader.h>
+
+
---
+++ CUPUS_iOS_MobileBrokerTests/CUPUS_iOS_MobileBrokerTests.swift
@@ -0,0 +1,36 @@
+//
+//  CUPUS_iOS_MobileBrokerTests.swift
+//  CUPUS_iOS_MobileBrokerTests
+//
+//  Created by Ivan Rep on 05/02/17.
+//  Copyright © 2017 CUPUS. All rights reserved.
+//
+
+import XCTest
+@testable import CUPUS_iOS_MobileBroker
+
+class CUPUS_iOS_MobileBrokerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
